bbfceb1e0063c56b4aa64f3b47cb0b315a003197
Matheus-Marques-de-oliveira <130381677+Matheus-Marques-de-oliveira@users.noreply.github.com>
2023-09-04 11:49:22 -0300

    alteração de titulo

---
+++ introducao-javascript/js/principal.js
@@ -0,0 +1,9 @@
+//-------------------------------------------------------------
+//daqui pra baixo começa o js do site
+
+//busca no site pelo titulo e armazena dentro da variavel titulo
+let titulo = document.querySelector(".titulo");
+//mostra no devtools o conteudo de texto da variavel titulo
+console.log(titulo.textContent);
+//altera o conteudo da variavel titulo para o texto desejado
+titulo.textContent = "Fica China pasta";
