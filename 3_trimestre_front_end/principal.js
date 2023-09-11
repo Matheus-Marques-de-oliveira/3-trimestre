9381de35ad71233ce489c7af84272358f32175a4
Matheus-Marques-de-oliveira <130381677+Matheus-Marques-de-oliveira@users.noreply.github.com>
2023-09-11 11:43:32 -0300

    mudou

--- 3_trimestre_front_end/principal.js
+++ 3_trimestre_front_end/principal.js
@@ -1,17 +1,20 @@
 //-------------------------------------------------------------
-//daqui pra baixo começa o js do site;
+//daqui pra baixo começa o js do site
 
-//busca no site pelo titulo e armazena dentro da variavel titulo;
+//busca no site pelo titulo e armazena dentro da variavel titulo
 let titulo = document.querySelector(".titulo");
-//mostra no devtools o conteudo de texto da variavel titulo;
+
+//mostra no devtools o conteudo de texto da variavel titulo
 console.log(titulo.textContent);
-//altera o conteudo da variavel titulo para o texto desejado;
+
+//altera o conteudo da variavel titulo para o texto desejado
 titulo.textContent = "Fica China pasta";
 
 //mudanças no titulo
-//armazena na variavel paciente os dados primeiros;
+//armazena na variavel paciente os dados primeiros
 let paciente = document.querySelector("#primeiro-paciente");
-//mostra on console  o valor da variavel;
+
+//mostra on console  o valor da variavel
 console.log("paciente");
 
 let tdPeso = paciente.querySelector("info-peso");
@@ -25,3 +28,15 @@ console.log(imc);
 
 let tdImc = paciente.querySelector(".info-imc");
 tdImc.textContent = imc;
+
+//validaçao de dados
+if(peso <= 0 || peso >= 1000){
+    console.log("Peso inválido!");
+}
+
+if(altura <= 0 || altura >= 3){
+    console.log("Altura inválida!");
+}
+//DESAFIO!!!
+//FAZER APARECER O ERRO "peso inválido"
+//NA CÉLULA DO IMC NA TABELA 
